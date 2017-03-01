4e4591bff55c98b42da0025273f35ddb84326132
Mike Schwörer <mailport@mikescher.de>
2017-03-01 18:06:55 +0100

    Version 1.1

--- Source/AlephNote.App/Properties/AssemblyInfo.cs
+++ Source/AlephNote.App/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
