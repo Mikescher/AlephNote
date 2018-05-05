a8ebd79aa6e333d42fe9365db38f8d328327083f
Mike Schwörer <mailport@mikescher.de>
2018-05-05 16:21:38 +0200

    fixed syntax highlighting for really long urls
    bumped URL highlighter MAX_BACKTRACE from 128 to 512 chars
    
    --> references #45

--- Source/AlephNote.App/WPF/Util/DefaultHighlighter.cs
+++ Source/AlephNote.App/WPF/Util/DefaultHighlighter.cs
@@ -6,14 +6,30 @@ namespace AlephNote.WPF.Util
 {
 	public class DefaultHighlighter : ScintillaHighlighter
 	{
-		private const int MAX_BACKTRACE = 128;
+		private const int MAX_BACKTRACE = 512;
+		private const int MAX_FORWARDTRACE = 512;
 		
 		public override void Highlight(Scintilla sci, int start, int end, AppSettings s)
 		{
+			bool startsWithNL = sci.GetCharAt(start) == '\n' || (start+1 < sci.TextLength && sci.GetCharAt(start) == '\r' && sci.GetCharAt(start+1) == '\n');
+			bool endsWithNL   = sci.GetCharAt(end) == '\n' || (end-1 >= 0 && sci.GetCharAt(end-1) == '\n');
+
 			// move back to start of line
-			for (int i = 0; i < MAX_BACKTRACE && start > 0; i++, start--)
+			if (!startsWithNL)
 			{
-				if (start > 0 && sci.GetCharAt(start - 1) == '\n') break;
+				for (int i = 0; i < MAX_BACKTRACE && start > 0; i++, start--)
+				{
+					if (start > 0 && sci.GetCharAt(start - 1) == '\n') break;
+				}
+			}
+			
+			// move forward to end of line
+			if (!endsWithNL)
+			{
+				for (int i = 0; i < MAX_FORWARDTRACE && end < sci.TextLength; i++, end++)
+				{
+					if (end >= sci.TextLength || sci.GetCharAt(end) == '\n') break;
+				}
 			}
 
 			var text = sci.GetTextRange(start, end - start);
