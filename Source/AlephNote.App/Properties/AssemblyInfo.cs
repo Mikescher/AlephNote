a8e6e0d891a13f506cd6845d8f1e6fca46c1956c
Mike Schwörer <mailport@mikescher.de>
2018-05-27 15:47:49 +0200

    version 1.6.17

--- Source/AlephNote.App/Properties/AssemblyInfo.cs
+++ Source/AlephNote.App/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.6.16.0")]
-[assembly: AssemblyFileVersion("1.6.16.0")]
-[assembly: AssemblyInformationalVersion("1.6.16.0-master")]
+[assembly: AssemblyVersion("1.6.17.0")]
+[assembly: AssemblyFileVersion("1.6.17.0")]
+[assembly: AssemblyInformationalVersion("1.6.17.0-master")]
