4fe518c03153a1c3fe7a3006078056ae6a0691db
Mike Schwörer <mailport@mikescher.de>
2018-10-24 16:37:52 +0200

    Fixed UpdateToPrerelease with strange github API responses

--- Source/AlephNote.Common/Operations/GithubConnection.cs
+++ Source/AlephNote.Common/Operations/GithubConnection.cs
@@ -11,38 +11,25 @@ namespace AlephNote.Common.Operations
 #pragma warning disable 0649
 // ReSharper disable All
 		private class JsonResponseAsset { public string browser_download_url; }
-		private class JsonResponse { public string tag_name; public DateTime published_at; public List<JsonResponseAsset> assets; }
+		private class JsonResponse { public string tag_name; public DateTime published_at; public List<JsonResponseAsset> assets; public bool prerelease; }
 // ReSharper restore All
 #pragma warning restore 0649
 
 		public Tuple<Version, DateTime, string> GetLatestRelease(bool includeBeta)
 		{
-			if (includeBeta)
-			{
-				var rest = new SimpleJsonRest(null, @"https://api.github.com");
+			var rest = new SimpleJsonRest(null, @"https://api.github.com");
+			var responses = rest
+				.Get<List<JsonResponse>>("repos/Mikescher/AlephNote/releases")
+				.OrderByDescending(p => Version.TryParse(p.tag_name.Trim('V', 'v', ' '), out var v) ? v : new Version(0, 0, 0))
+				.ToList();
 
-				var response = rest.Get<List<JsonResponse>>("repos/Mikescher/AlephNote/releases").First();
+			var response = includeBeta ? responses.First() : responses.First(p => !p.prerelease);
+			
+			var url     = response.assets.First(a => a.browser_download_url.EndsWith(".zip")).browser_download_url;
+			var date    = response.published_at;
+			var version = ParseVersion(response.tag_name);
 
-				var url = response.assets.First(a => a.browser_download_url.EndsWith(".zip")).browser_download_url;
-				var date = response.published_at;
-				var version = ParseVersion(response.tag_name);
-
-				return Tuple.Create(version, date, url);
-			}
-			else
-			{
-				var rest = new SimpleJsonRest(null, @"https://api.github.com");
-
-				var response = rest.Get<JsonResponse>("repos/Mikescher/AlephNote/releases/latest");
-
-				var url = response.assets.First(a => a.browser_download_url.EndsWith(".zip")).browser_download_url;
-				var date = response.published_at;
-				var version = ParseVersion(response.tag_name);
-
-				return Tuple.Create(version, date, url);
-			}
-
-
+			return Tuple.Create(version, date, url);
 		}
 
 		private static Version ParseVersion(string dat)
