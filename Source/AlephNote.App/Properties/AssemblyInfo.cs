9c0c5fde13449e4f263c3d9569a715a828a0eb93
Mike Schwörer <mailport@mikescher.de>
2018-12-14 20:48:50 +0100

    Release version 1.6.26
    --> fixes #82
    --> fixes #83
    --> fixes #85
    --> fixes #88
    --> fixes #89
    --> fixes #90

--- Source/AlephNote.App/Properties/AssemblyInfo.cs
+++ Source/AlephNote.App/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.6.25.0")]
-[assembly: AssemblyFileVersion("1.6.25.0")]
-[assembly: AssemblyInformationalVersion("1.6.25.0-master")]
+[assembly: AssemblyVersion("1.6.26.0")]
+[assembly: AssemblyFileVersion("1.6.26.0")]
+[assembly: AssemblyInformationalVersion("1.6.26.0-master")]
