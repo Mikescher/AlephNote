36fd069d5edeca01dd87ad5bcafbbb6bc2bbcaa5
Mike Schwörer <mailport@mikescher.de>
2019-03-24 00:29:07 +0100

    Release version 1.6.29

--- Source/AlephNote.App/Properties/AssemblyInfo.cs
+++ Source/AlephNote.App/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.6.28.0")]
-[assembly: AssemblyFileVersion("1.6.28.0")]
-[assembly: AssemblyInformationalVersion("1.6.28.0-master")]
+[assembly: AssemblyVersion("1.6.29.0")]
+[assembly: AssemblyFileVersion("1.6.29.0")]
+[assembly: AssemblyInformationalVersion("1.6.29.0-master")]
