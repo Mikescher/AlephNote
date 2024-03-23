59b14ebfeb159c2a40f578ea1899cb9262e33fb4
Mike Schwörer <pubgit@mikescher.com>
2024-03-24 00:12:51 +0100

    Fix build, Add `AlephNote.GitBackupService` project

---
+++ Source/AlephNote.GitBackupService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlephNote.GitBackupService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started");
+        }
+    }
+}
